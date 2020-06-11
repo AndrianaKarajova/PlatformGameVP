cbf227e661bdacce0afbcc648ba2d4079123102c
AndrianaKarajova <akarajova@gmail.com>
2020-06-11 22:41:02 +0200

    Added movement right,left and jumping

---
+++ PlatformGame/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlatformGame
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft, goRight, jumping, isGameOver;
+        int jumpSpeed;
+        int force;
+        int score = 0;
+        int playerSpeed = 7;
+
+        int horizontalSpeed = 5;
+        int verticalSpeed = 3;
+        int enemyOneSpeed = 5;
+        int enemyTwoSpeed = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+            txtScore.Text = "Score: " + score;
+            player.Top += jumpSpeed;
+
+            if(goLeft == true)
+            {
+                player.Left -= playerSpeed;
+            
+            }
+            if(goRight == true)
+            {
+                player.Left += playerSpeed;
+            }
+            if(jumping == true && force < 0)
+            {
+                jumping = false;
+            
+            }
+            if(jumping == true)
+            {
+                jumpSpeed = -8;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 10;
+            }
+
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox)
+                {
+                    if ((string)x.Tag == "platform")
+                    {
+                        if (player.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            force = 8;
+                            player.Top = x.Top - player.Height;
+                        }
+                        x.BringToFront();
+                    }
+                }
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+            if(e.KeyCode == Keys.Space)
+            {
+                jumping = true;
+            }
+            //if(e.KeyCode == Keys.Space && jumping == true)
+           // {
+            //    jumping = true;
+            //}
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (jumping == true)
+            {
+                jumping = false;
+            }
+            if(e.KeyCode==Keys.Enter && isGameOver == true)
+            {
+                RestartGame();
+            }
+
+        }
+
+        private void RestartGame()
+        {
+            jumping = false;
+            goLeft = false;
+            goRight = false;
+            isGameOver = false;
+            score = 0;
+
+            txtScore.Text = "Score: " + score;
+
+            // treba parickite da se pojavat povtorno dokolku korisnikot prethodno gi ima zemeno
+            foreach (Control x in this.Controls)
+            {
+                if(x is PictureBox && x.Visible == false)
+                {
+                    x.Visible = true;
+                }
+            }
+
+            //treba da se namesti pozicijata na player-ot,enemies i na platforms na pocetok
+            player.Left = 12;
+            player.Top = 676;
+
+            enemyOne.Left = 575;
+            enemyTwo.Left = 352;
+
+            horizontalPlatform.Left = 249;
+            verticalPlatform.Top = 545;
+
+            gameTimer.Start();
+
+        }
+
+    }
+}
